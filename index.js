1e7e25ac491baaa348a77745ec6ece553bdbc432
Harshana Malaka <harshanamaduwanthasilva@gmail.com>
2025-01-01 21:59:16 +0530

    Adding simple css styles

--- index.js
+++ index.js
@@ -17,11 +17,21 @@ function search(){
         
        console.log(htmlreqest.response)
 
-       const imageTag = document.getElementById("poster")
-       imageTag.src = htmlreqest.response.Poster
+        const imageTag = document.getElementById("poster")
+        imageTag.src = htmlreqest.response.Poster
 
-       const rateTag = document.getElementById("rate")
+        const titleTag = document.getElementById("title")
+        titleTag.innerHTML = htmlreqest.response.Title
+
+        const rateTag = document.getElementById("rate")
         rateTag.innerHTML = htmlreqest.response.imdbRating
+
+        const directTag = document.getElementById("director")
+        directTag.innerHTML = htmlreqest.response.Director
+
+        const BoxTag = document.getElementById("boffice")
+        BoxTag.innerHTML = htmlreqest.response.BoxOffice
+
 
 
     }
